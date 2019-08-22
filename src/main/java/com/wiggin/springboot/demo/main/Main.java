c0c01232d3a3fc9e6cfcdfb9ce390255fc0c1725
ydkj <chenbo@chinaydkj.com>
2019-08-22 14:13:11 +0800

    v2

--- src/main/java/com/wiggin/springboot/demo/main/Main.java
+++ src/main/java/com/wiggin/springboot/demo/main/Main.java
@@ -22,5 +22,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
+        System.out.println("v2");
     }
 }
